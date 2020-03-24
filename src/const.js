9105f5e13dbb1628e1f388a8c395dc25bce0c710
thaitwo <tmtu90@gmail.com>
2020-03-23 22:32:41 -0700

    Change tokens to PROD

--- src/const.js
+++ src/const.js
@@ -1,10 +1,10 @@
 /*** PRODUCTION ENVIRONMENT ***/
-// const URL_BASE = 'https://cloud.iexapis.com/v1/stock';
-// const API_TOKEN = 'pk_a12f90684f2a44f180bcaeb4eff4086d';
+const URL_BASE = 'https://cloud.iexapis.com/v1/stock';
+const API_TOKEN = 'pk_a12f90684f2a44f180bcaeb4eff4086d';
 
 /*** SANDBOX ENVIRONMENT ***/
-const URL_BASE = 'https://sandbox.iexapis.com/v1/stock';
-const API_TOKEN = 'Tpk_8d25ed3be77a45ddb7919558e730257f';
+// const URL_BASE = 'https://sandbox.iexapis.com/v1/stock';
+// const API_TOKEN = 'Tpk_8d25ed3be77a45ddb7919558e730257f';
 
 export {
   URL_BASE,
